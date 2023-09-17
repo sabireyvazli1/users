23b790aae5d3bbc0759ee040a0de6338b8aa912a
sabireyvazli1 <sabireyvazli@gmail.com>
2023-09-17 12:06:53 +0400

    done

--- app.js
+++ app.js
@@ -1,61 +1,69 @@
 const express = require("express");
+const mysql = require("mysql2");
+var cors = require('cors')
 const app = express();
-const port = process.env.PORT || 3001;
+app.use(express.json());
+app.use(cors())
+const PORT = 3306;
+const connection = mysql.createConnection({
+  host: "bk5saonxmjfbaitb1ulq-mysql.services.clever-cloud.com",
+  user: "ukbs6uwyue4gosbg",
+  password: "y0qRCt4xT1MXYawB4DYo",
+  database: "bk5saonxmjfbaitb1ulq",
+});
 
-app.get("/", (req, res) => res.type('html').send(html));
+app.get("/student",(req, res) => {
+  connection.query("select * from users", function (err, result, fields) {
+    console.log(result);
+    res.send(result);
+  });
+});
+app.get("/student/:id", (req, res) => {
+  const elem = req.params;
+  let found = false; // Flag to track if a matching record is found
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+  connection.query("select * from users", function (err, result, fields) {
+    for (let i = 0; i < result.length; i++) {
+      if (elem.id == result[i].id) {
+        found = true; // Set the flag to true if a match is found
+        res.send(result[i]); // Send the response for the matching record
+        break; // Exit the loop when a match is found
+      }
+    }
+    // If no match is found, send an appropriate response
+    if (!found) {
+      res.status(404).send("User not found!"); // Use a 404 status code for not found
+    }
+  });
+});
 
-server.keepAliveTimeout = 120 * 1000;
-server.headersTimeout = 120 * 1000;
+app.delete("/student/:id", (req, res) => {
+  const idToDelete = req.params.id;
 
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
-      }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
-</html>
-`
+  connection.query(
+    `DELETE FROM users WHERE id = ?`,
+    [idToDelete],
+    function (err, result, fields) {
+    }
+  );
+  connection.query("select * from users", function (err, result, fields) {
+    console.log(result);
+    res.send(result);
+  });
+});
+
+
+app.post("/student/", (req, res) => {
+  let obj = req.body;
+  connection.query(
+    `INSERT INTO users (id, ad, soyad, parol, adress)
+    VALUES ("${obj.id}", "${obj.ad}", "${obj.soyad}", "${obj.parol}")`,
+    function (err, result, fields) {
+    }
+  );
+  connection.query("select * from users", function (err, result, fields) {
+    console.log(result);
+    res.send(result);
+  });
+});
+app.listen(process.env.PORT || 3000);
